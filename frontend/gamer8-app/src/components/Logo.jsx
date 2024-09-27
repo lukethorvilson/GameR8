c90a6613b2ad30fd17b2a641cd2831089d10f0b1
lukethorvilson <luke.thorvilson@wsu.edu>
2024-09-27 14:23:01 -0700

    Added all recent changes.

---
+++ frontend/gamer8-app/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>R8</div>
+  )
+}
+
+export default Logo
---
+++ frontend/gamer8-app/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="mx-auto h-20 w-[100%] bg-cyan-950 text-base text-center">
+        
+    </div>
+  );
+}
+
+export default NavBar;
