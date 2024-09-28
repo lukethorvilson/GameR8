6087e62137dba5dc26bab2ca8f3a01e16f0f2321
Luke Thorvilson <luke.thorvilson@wsu.edu>
2024-09-27 17:17:28 -0700

    Added some more styling to the login.

--- frontend/gamer8-app/src/components/LoginBox.jsx
+++ frontend/gamer8-app/src/components/LoginBox.jsx
@@ -2,25 +2,37 @@ import React from "react";
 
 function LoginBox() {
   return (
-    <div className="mx-auto flex h-[75%] w-[45%] rounded-xl flex-col bg-cyan-950 px-4 py-8">
-      <h1 className="text-center text-yellow-300 text-2xl font-bold mb-10">Login</h1>
-      <label htmlFor="username" className="text-yellow-300">
-        Username or Email
+    <div className="mx-auto flex h-[60%] w-[45%] flex-col rounded-xl bg-cyan-950 px-4 py-8">
+      <h1 className="mb-10 text-center text-2xl font-bold text-yellow-300">
+        Login
+      </h1>
+      <label
+        htmlFor="username"
+        className="mx-auto font-semibold text-yellow-300"
+      >
+        Username
       </label>
       <input
         id="username"
-        placeholder="username or email"
-        className="bg-cyan-800 placeholder-yellow-300 placeholder-opacity-70"
+        placeholder="username | email"
+        className="mx-auto mb-8 h-[6%] w-[80%] rounded-md bg-cyan-800 px-2 placeholder-yellow-300 placeholder-opacity-70"
       />
 
-      <label htmlFor="username" className="text-yellow-300">
-        Username or Email
+      <label
+        htmlFor="password"
+        className="mx-auto font-semibold text-yellow-300"
+      >
+        Password
       </label>
       <input
-        id="username"
+        id="password"
+        type="password"
         placeholder="username or email"
-        className="bg-cyan-800 placeholder-yellow-300 placeholder-opacity-70"
+        className="mx-auto mb-8 h-[6%] w-[80%] rounded-md bg-cyan-800 px-2 placeholder-yellow-300 placeholder-opacity-70"
       />
+      <button className="mx-auto h-[8%] w-[20%] rounded-md bg-yellow-300 font-bold text-cyan-950">
+        Login
+      </button>
     </div>
   );
 }
