50894489763138e12d69764d4e654ded58a060e3
lukethorvilson <luke.thorvilson@wsu.edu>
2024-09-29 22:24:32 -0700

    Changed the salt rounds to 12 rounds for better security.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,70 +1,74 @@
 const sequelize = require("../db");
 const { Sequelize, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  likedReviews: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: "",
-    get() {
-      return this.getDataValue("likedReviews").split(",");
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
     },
-    set(val) {
-      this.setDataValue("likedReviews", val.join(","));
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likedReviews: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "",
+      get() {
+        return this.getDataValue("likedReviews").split(",");
+      },
+      set(val) {
+        this.setDataValue("likedReviews", val.join(","));
+      },
+    },
+    dislikedReviews: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "",
+      get() {
+        return this.getDataValue("dislikedReviews").split(",");
+      },
+      set(val) {
+        this.setDataValue("dislikedReviews", val.join(","));
+      },
     },
   },
-  dislikedReviews: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: "",
-    get() {
-      return this.getDataValue("dislikedReviews").split(",");
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const saltRounds = 12;
+          user.password = await bcrypt.hash(user.password, saltRounds);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.password) {
+          const saltRounds = 12;
+          user.password = await bcrypt.hash(user.password, saltRounds);
+        }
+        before;
+      },
     },
-    set(val) {
-      this.setDataValue("dislikedReviews", val.join(","));
-    },
-  },
-}, {
-  hooks: {
-    beforeCreate: async(user) => {
-      if(user.password) {
-        const saltRounds = 10;
-        user.password = await bcrypt.hash(user.password, saltRounds);
-      }
-    },
-    beforeUpdate: async (user) => {
-      if(user.password) {
-        const saltRounds = 10;
-        user.password = await bcrypt.hash(user.password, saltRounds);
-      }
-    before
-    }
-  },
-});
+  }
+);
 
 module.exports = User;
