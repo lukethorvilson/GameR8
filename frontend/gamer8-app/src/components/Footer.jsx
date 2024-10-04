a67bfa55820b4760b1017928b9ef68811e171055
Luke Thorvilson <luke.thorvilson@wsu.edu>
2024-10-03 19:33:11 -0700

    Added the footer RAWG games link for api useage.

--- frontend/gamer8-app/src/components/Footer.jsx
+++ frontend/gamer8-app/src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='absolute bottom-0 bg-cyan-950 w-screen h-[15vh]'></div>
-  )
+    <div className="absolute bottom-0 flex h-[15vh] w-screen bg-cyan-950">
+      <p className="mx-auto my-auto align-middle font-bold text-yellow-300">
+        Powered by
+        <a className="ml-2 text-2xl hover:text-[26px] transition-all font-extrabold text-white" href="https://rawg.io/">R A W G</a>
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
